a47a344291a48be61d1289ad542eef50add568e1
진민관 <chin0982@naver.com>
2025-05-08 14:03:54 +0900

    투두 리스트 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [toDo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo===""){ 
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setTodo("");
+
+  }
+  console.log(toDos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Do List  [{toDos.length}]</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write you to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      <ul>{toDos.map((item,index)=>(
+        <li key={index}>{item}</li>))}</ul>
     </div>
   );
 }
